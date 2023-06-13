122ff11655582264d468a51a023cfac6bde0ee02
olugbengalium <olugbengalium@gmail.com>
2023-06-13 06:06:39 +0100

    Initial Commit

---
+++ ESDiac_Auth/ESDiac_AuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  ESDiac_AuthApp.swift
+//  ESDiac_Auth
+//
+//  Created by Gbenga Abayomi on 13/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ESDiac_AuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
